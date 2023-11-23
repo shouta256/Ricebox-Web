6c61476250edf15b77a1fac36c70c580e09237c2
shouta256 <shouta160618@gmail.com>
2023-11-23 20:23:17 +0900

    フッターとヘッダーの作成

---
+++ components/Footer.jsx
@@ -0,0 +1,26 @@
+import styles from "components/Footer.module.css";
+import Link from "next/link";
+
+export const Footer = () => {
+  return (
+    <footer className={styles.Footer}>
+      <div className={styles.main}>
+        <img className={styles.logo} src="textLogo.png"></img>
+        <div className={styles.links}>
+          <Link className={styles.link} href="./about">
+            About
+          </Link>
+
+          <Link className={styles.link} href="./service">
+            Service
+          </Link>
+          <Link className={styles.link} href="./news">
+            News
+          </Link>
+        </div>
+      </div>
+
+      <p className={styles.copyRight}>Copyright@Ricebox</p>
+    </footer>
+  );
+};
---
+++ components/Header.jsx
@@ -0,0 +1,23 @@
+import styles from "components/Header.module.css";
+import Link from "next/link";
+
+export const Header = () => {
+  return (
+    <div>
+      <header className={styles.Header}>
+        <img className={styles.logo} src="textLogo.png"></img>
+        <div className={styles.links}>
+          <Link className={styles.link} href="./about">
+            About
+          </Link>
+          <Link className={styles.link} href="./service">
+            Service
+          </Link>
+          <Link className={styles.link} href="./news">
+            News
+          </Link>
+        </div>
+      </header>
+    </div>
+  );
+};
