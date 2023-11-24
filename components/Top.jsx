bc4bde0416db4fa29178153ddac278f3042a619a
shouta256 <shouta160618@gmail.com>
2023-11-24 16:23:07 +0900

    propsの導入

--- components/Top.jsx
+++ components/Top.jsx
@@ -1,14 +1,9 @@
 import styles from "components/Top.module.css";
 
-export const Top = () => {
+export const Top = (props) => {
   return (
     <div className={styles.body}>
-      <h1 className={styles.message}>
-        すべての人に無駄なく
-        <br />
-        より安く、より良い
-        <br /> 商品を届ける
-      </h1>
+      <h1 className={styles.message}>{props.title}</h1>
     </div>
   );
 };
