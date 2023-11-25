b69875222c6a78d8c88923cafcef0e4fa86b9315
shouta256 <shouta160618@gmail.com>
2023-11-25 20:55:57 +0900

    汎用化

--- components/Topic.jsx
+++ components/Topic.jsx
@@ -4,11 +4,31 @@ import Link from "next/link";
 export const Topic = (props) => {
   return (
     <div className={styles.body}>
-      <Link href="/about">
-        <img className={styles.image} src="logo.jpg"></img>
-        <h2 className={styles.title}>{props.title}</h2>
-        <p className={styles.description}>{props.description}</p>
-      </Link>
+      {props.page ? (
+        <Link href={props.page}>
+          <div className={styles.imageContainer}>
+            <img
+              className={styles.image}
+              src={props.imageName}
+              alt={props.title}
+            />
+          </div>
+          <h2 className={styles.title}>{props.title}</h2>
+          <p className={styles.description}>{props.description}</p>
+        </Link>
+      ) : (
+        <>
+          <div className={styles.imageContainer}>
+            <img
+              className={styles.image}
+              src={props.imageName}
+              alt={props.title}
+            />
+          </div>
+          <h2 className={styles.title}>{props.title}</h2>
+          <p className={styles.description}>{props.description}</p>
+        </>
+      )}
     </div>
   );
 };
