2f9da35cb7bfe3bcd100506d582f37b51a274142
shouta256 <shouta160618@gmail.com>
2023-11-25 20:55:34 +0900

    descriptionの追加

--- components/Top.jsx
+++ components/Top.jsx
@@ -4,6 +4,7 @@ export const Top = (props) => {
   return (
     <div className={styles.body}>
       <h1 className={styles.message}>{props.title}</h1>
+      <p className={styles.description}>{props.description}</p>
     </div>
   );
 };
