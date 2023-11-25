0326104592e37191434604dfdde490e36f835b79
shouta256 <shouta160618@gmail.com>
2023-11-25 11:51:56 +0900

    NewsContentの作成

---
+++ components/NewsContent.jsx
@@ -0,0 +1,10 @@
+import styles from "components/NewsContent.module.css";
+
+export const NewsContent = (props) => {
+  return (
+    <div className={styles.body}>
+      <img className={styles.image} src={props.imgName} alt="News Image"></img>
+      <p>{props.description}</p>
+    </div>
+  );
+};
