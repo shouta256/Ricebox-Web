f0b02e492ecd3a145738eba911b50d406e788c52
Shouta256 <shouta160618@gmail.com>
2023-11-30 04:25:18 +0900

    Meritsとの切り離しとレスポンシブ対応

--- components/Topic.jsx
+++ components/Topic.jsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 export const Topic = (props) => {
   return (
     <div className={styles.body}>
-      {props.page ? (
         <Link href={props.page}>
           <div className={styles.imageContainer}>
             <img
@@ -16,19 +15,6 @@ export const Topic = (props) => {
           <h2 className={styles.title}>{props.title}</h2>
           <p className={styles.description}>{props.description}</p>
         </Link>
-      ) : (
-        <>
-          <div className={styles.imageContainer}>
-            <img
-              className={styles.image}
-              src={props.imageName}
-              alt={props.title}
-            />
-          </div>
-          <h2 className={styles.title}>{props.title}</h2>
-          <p className={styles.description}>{props.description}</p>
-        </>
-      )}
     </div>
   );
 };
