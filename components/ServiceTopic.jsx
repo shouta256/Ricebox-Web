19fcd7cfd70868299104424d5a7f8c0556f85739
Shouta256 <shouta160618@gmail.com>
2023-11-30 04:24:51 +0900

    アニメーションの追加とレスポンシブ対応

--- components/ServiceTopic.jsx
+++ components/ServiceTopic.jsx
@@ -1,8 +1,16 @@
+import { Merits } from "components/Merits";
 import styles from "components/ServiceTopic.module.css";
 import { Topic } from "components/Topic";
+import { useInView } from 'react-intersection-observer';
 
 export const ServiceTopic = (props) => {
+  const [ref, inView] = useInView({
+    rootMargin: '-100px',
+    triggerOnce: true,
+  });
   return (
+    <div ref={ref} className={`${styles.fadeIn} ${inView ? styles.visible : ''}`}>
+
     <div className={styles.body}>
       <h2 className={styles.title}>{props.title}</h2>
       <p className={styles.description}>{props.description}</p>
@@ -14,13 +22,14 @@ export const ServiceTopic = (props) => {
       <div className={styles.merits}>
         {Array.isArray(props.merits) &&
           props.merits.map((merit, index) => (
-            <Topic
+            <Merits
               key={index}
               imageName={merit.imageName}
               description={merit.description}
             />
           ))}
       </div>
+    </div>
     </div>
   );
 };
